e73e4879b563af5811d4f8ed261273a66d508285
Muthuprasanth Murugesan <muthuprasanthm@INLM-MuthP.local>
2018-04-12 18:40:51 +0530

    03 commit

--- api/routes/todolistroutes.js
+++ api/routes/todolistroutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
   // todoList Routes
   app.route('/tasks')
-    .post(todoList.list_all_tasks);
+    .get(todoList.list_all_tasks);
    // .post(todoList.create_a_task);
 
 
