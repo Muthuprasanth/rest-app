8c8babb7b9a4a7d3e1473a343809e12d5389a354
Muthuprasanth Murugesan <muthuprasanthm@INLM-MuthP.local>
2018-04-18 19:41:14 +0530

    08 commit

--- api/controllers/todolistcontroller.js
+++ api/controllers/todolistcontroller.js
@@ -7,7 +7,7 @@ var sppull = require("sppull").sppull;
 //var mongoose = require('mongoose'),
  // Task = mongoose.model('Tasks');
 
-exports.list_all_tasks = function(req, res) {
+exports.list_all_tasks = async function(req, res) {
 
 //console.log(process.env.SITE_PASSWORD);
  var context = {
@@ -27,7 +27,7 @@ exports.list_all_tasks = function(req, res) {
 
 const directory = 'Resumes';
 
-fs.readdir(directory, (err, files) => {
+ await fs.readdir(directory, (err, files) => {
   if (err) throw err;
 
   for (const file of files) {
@@ -42,7 +42,7 @@ fs.readdir(directory, (err, files) => {
 });
 
 
-    sppull(context, options)
+ await  sppull(context, options)
     .then(function(downloadResults) {
       console.log("Files are downloaded");
       fs.readdir('./Resumes', function(err, items) {
@@ -51,7 +51,7 @@ fs.readdir(directory, (err, files) => {
 
           textract.fromFileWithPath('./Resumes/'+items[i], function( error, text ) {
 
-            res.json({ message: 'Files are downloaded' });
+           // res.json({ message: 'Files are downloaded' });
             //nlpParser(text);
             console.log("file data",text);
           
@@ -59,6 +59,7 @@ fs.readdir(directory, (err, files) => {
         }
         console.log("finished");
         })
+      res.json({ message: 'Files are downloaded' });
     })
     .catch(function(err) {
     //  console.log("Core error has happened", err);
